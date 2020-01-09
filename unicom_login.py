5317dc3c62042c8d046cf3acfe1b8863c56e8459
Syu <36406079+Syucryingstar@users.noreply.github.com>
2020-01-09 11:02:52 +0800

    优化
    优化

--- unicom_login.py
+++ unicom_login.py
@@ -6,28 +6,6 @@ import requests
 from lxml import etree
 
 
-def get_ip():
-    """
-    获取内网ip, 错误则返回字符串"unknow"
-    :return:
-    """
-    local_ip = "unknow"
-    try:
-        info = psutil.net_if_addrs()
-        for k, v in info.items():
-            for item in v:
-                if item[0] == 2 and not item[1] == '127.0.0.1':
-                    if "10." == item[1][:3]:
-                        local_ip = item[1]
-                        break
-                    # netcard_info.append((k, item[1]))  # 所有网卡名称和ip
-    except Exception as error:
-        print("获取ip失败")
-        print(error)
-    finally:
-        return local_ip
-
-
 def unicom_login(username, password, login_out=False):
     """
     广东联通校园宽带登录
@@ -36,7 +14,10 @@ def unicom_login(username, password, login_out=False):
     :param login_out: 是否下线该账号
     :return:
     """
-    ip = get_ip()  # 获取内网ip
+    response_info = requests.get("http://2.2.2.2", timeout=3)
+    redit_list = response_info.history  # 获取联通重定向历史信息
+    baspushurl = redit_list[len(redit_list)-1].headers["location"]  # 获取重定向url
+    ip = baspushurl.split("&")[0].split("=")[1]  # 获取内网ip
     print(ip)
     url = 'http://portal.gd165.com/login.do'
     headers = {
@@ -64,8 +45,7 @@ def unicom_login(username, password, login_out=False):
         "customerId": "001",
         "customerName": "campus",
         "basName": "120.80.172.172",
-        "basPushUrl": "http://portal.gd165.com/?wlanuserip={}&wlanacname=&basname=120.80.172.172&ssid=capus.gz&vlanid="
-                      "ethtrunk/2:1643.0".format(ip),
+        "basPushUrl": baspushurl,
         "attrName": "ssid",
         "attrValue": "[capus.gz]",
         "wlanuserip": ip,
@@ -76,7 +56,7 @@ def unicom_login(username, password, login_out=False):
         "accountType": "fyhtc"
     }
 
-    if login_out:
+    if login_out:  # 退出登录
         url = "http://portal.gd165.com/logout.do"
         headers["Referer"] = "http://portal.gd165.com/login.do"
         data["errormessage"] = "success"
